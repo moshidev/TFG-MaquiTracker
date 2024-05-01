6774775a5c4d9a379c981d29174a2fea2dec508e
Daniel P <46326833+moshidev@users.noreply.github.com>
2024-05-01 23:41:10 +0200

    añade motivaciones en los scripts (closes #4)

--- dev-config.sh
+++ dev-config.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+# El propósito de este script es configurar git
+# para que utilice los hooks que se encuentran
+# en el directorio .git-hooks
 
 git config core.hooksPath $(pwd)/.git-hooks
